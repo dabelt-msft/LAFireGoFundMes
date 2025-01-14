813be95cdda1f1e90c41bd08e6124b3dbd7a91e3
DAVID BELTRAN <davidbeltran@DAVIDs-MBP.lan>
2025-01-13 16:33:59 -0800

    removes unused import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { use, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import type { NextPage } from "next";
 import initialData from "../data/gofundme-data.json";
 import Link from "next/link";
